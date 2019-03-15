8e0e5a97f270c3e6d64c0bd50f4f3b1273d55bcd
mike02140 <mike02140@gmail.com>
2019-03-14 20:53:32 -0400

    Update make_input_for_nmf.m

--- src/make_input_for_nmf.m
+++ src/make_input_for_nmf.m
@@ -3,10 +3,6 @@ function X = make_input_for_nmf(m,P)
 if ~exist('P','var'), P=[]; end
 P = impose_default_value(P,'coding_only',true);
 
-gene_strand_info_file = 'coverage_models.v5a.mat.genes.txt';
-G = load_struct(gene_strand_info_file);
-demand_fields(G,{'gene','strand'});
-
 if ischar(m)
   if exist(m,'dir')
     m = load2(m);
@@ -55,19 +51,6 @@ else
   m.is_snp = ~isnan(m.newbase_idx);
 end
 
-% flip (-)-strand mutations 
-if isfield(m,'gene')
-  rc('ACGT')='TGCA';
-  orig_list = parsein(orig_list,'name','^(.) in (.)_(.)$',{'from','left','right'});
-  orig_list.rc = repmat({''},65,1);
-  for i=1:64, orig_list.rc{i,1} = [rc(orig_list.from{i}) ' in ' rc(orig_list.right{i}) '_' rc(orig_list.left{i})]; end
-  orig_list.rcmap = listmap(orig_list.rc,orig_list.name);
-  m.strand = mapacross(m.gene,G.gene,G.strand);
-  midx = find(strcmp('-',m.strand) & m.is_snp);
-  m.newbase_idx(midx) = mapacross(m.newbase_idx(midx),[1 2 3 4],[4 3 2 1]);
-  m.context65(midx) = mapacross(m.context65(midx),1:65,orig_list.rcmap);
-end
-
 % patients
 [tmp pati m.pat_idx] = unique(m.patient);
 pat = reorder_struct(keep_fields_if_exist(m,{'patient','ttype','dataset','source','repo','ttype0'}),pati);
@@ -109,7 +92,6 @@ X=[];
 X.pat = rmfield(pat,'nsnp');
 X.chan = chan;
 
-% ADDED THESE STEPS HERE:
 X = collapse_nmf_input_192_to_96(X);
 X.chan.catnames = regexprep(X.chan.name,'^(.) \((.)->(.)\) (.)$','$2 in $1_$4 ->$3');
 
@@ -120,38 +102,4 @@ if ~isempty(idx)
   fprintf('WARNING: removing %d/%d patients that have zero mutations.\n',length(idx),slength(X.pat));
   X.pat = reorder_struct_exclude(X.pat,idx);
 end
-
-
-return
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-% post-processing code:
-
-X = make_input_for_nmf('/cga/tcga-gsc/home/lawrence/mut/20140218_pancan/data.v6.1.supermaf');
-X.pat = rmfield(X.pat,{'nsnp','num'});
-X.pat.n = cat(2,X.pat.nchan,X.pat.ndel,X.pat.nins,X.pat.ndnp);
-X.pat = rmfield(X.pat,{'nchan','ndel','nins','ndnp'});
-X.chan.name = [X.chan.name;'DEL';'INS';'DNP'];
-pat = rmfield(X.pat,{'source','repo','n'});
-pat.num = (1:slength(pat))';
-pat = order_fields_first(pat,'num');
-n = X.pat.n;
-chan = X.chan;
-chan.num = (1:slength(chan))';
-chan = order_fields_first(chan,'num');
-ede('/cga/tcga-gsc/home/lawrence/mut/20140218_pancan/data.v6.1.supermaf.NMF');
-save_struct(pat,'/cga/tcga-gsc/home/lawrence/mut/20140218_pancan/data.v6.1.supermaf.NMF/patients.txt');
-save_struct(chan,'/cga/tcga-gsc/home/lawrence/mut/20140218_pancan/data.v6.1.supermaf.NMF/channels.txt');
-save_matrix(n,'/cga/tcga-gsc/home/lawrence/mut/20140218_pancan/data.v6.1.supermaf.NMF/counts.txt');
-
-
-
-% code used to make "gene_strand_info_file"
-
-tmp = load('/xchip/cga/reference/mutsig_params/coverage_models.v5a.mat');
-G = keep_fields(tmp.C.gene,{'name','chr','tx_start','tx_end','strand'})
-G = rename_field(G,'name','gene')
-save_struct(G,'/xchip/cga/reference/mutsig_params/coverage_models.v5a.mat.genes.txt');
-
 
